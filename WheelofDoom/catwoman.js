f88c525b6aff311727e214da1d9356b08837e368
Manuraptor <majejuma.com>
2023-03-28 14:42:49 +0200

    pruebamaqueta

---
+++ WheelofDoom/catwoman.js
@@ -0,0 +1,17 @@
+function efectlatigazo(){
+
+    document.getElementById("latigazo") .play();
+}
+
+function efectlatigazo1(){
+
+    document.getElementById("latigazo1") .play();
+}
+
+
+function efectlatigazo2(){
+
+    document.getElementById("latigazo2") .play();
+}
+
+
