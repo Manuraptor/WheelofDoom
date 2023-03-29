6e8cf4bc2d1d92dcb586bb7bce956a816535bbbe
Manuraptor <majejuma.com>
2023-03-29 11:31:12 +0200

    prueba29m

---
+++ catwoman.js
@@ -0,0 +1,15 @@
+function efectlatigazo() {
+
+    document.getElementById("latigazo").play();
+}
+
+
+
+/*function lluvia() {
+
+    document.getElementById("efecto").play();
+}
+
+function paralluvia() {
+    document.getElementById("efecto").pause();
+}*/
