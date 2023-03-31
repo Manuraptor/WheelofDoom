87191f8eff37f1c85b4a567b9f4989b1bc4cb205
Nadiasolsol <nada.butnik@gmail.com>
2023-03-31 13:04:33 +0200

    Modo noche

--- catwoman.js
+++ catwoman.js
@@ -6,12 +6,36 @@ function efectlatigazo() {
 }
 
 
+/* Modo noche
+
+Si clicamos en el botón del sol, borrarémos la clase css dark-mode del section 
+con id container y se aplicará el estilo active al sol*/
+document.getElementById('id-sun').onclick = function(){
+    document.getElementById('container').classList.remove('dark-mode')
+    document.getElementById('id-moon').classList.remove('active')
+    this.classList.add('active')
+}
+  /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del section 
+  con id container y se aplicará el estilo active a la luna*/
+  document.getElementById('id-moon').onclick = function(){
+    document.getElementById('container').classList.add('dark-mode')
+    document.getElementById('id-sun').classList.remove('active')
+    this.classList.add('active')
+}
 
 function lluvia() {
 
-    document.getElementById("efecto").play();
+    var sonido=document.getElementById("efecto");
+    
+    
+        if (sonido.paused) {
+            sonido.play();
+            
+         } else {
+    
+            sonido.pause(); 
+    }
 }
-
 
 /*function paralluvia() {
     document.getElementById("efecto").pause();
