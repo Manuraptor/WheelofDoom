f3af81b67248b3a10b74e6288495184e452c88ba
Manuraptor <majejuma.com>
2023-04-11 10:43:34 +0200

    musica1

---
+++ Wheel of Doom/catwoman.js
@@ -0,0 +1,57 @@
+function efectlatigazo() {
+
+    document.getElementById("latigazo").play();
+
+    document.getElementById("animacion").play();
+}
+
+
+/* Modo noche
+
+Si clicamos en el botón del sol, borrarémos la clase css dark-mode del section 
+con id container y se aplicará el estilo active al sol*/
+document.getElementById('id-sun').onclick = function(){
+    document.getElementById('container').classList.remove('dark-mode')
+    document.getElementById('id-moon').classList.remove('active')
+    this.classList.add('active')
+}
+  /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del section 
+  con id container y se aplicará el estilo active a la luna*/
+  document.getElementById('id-moon').onclick = function(){
+    document.getElementById('container').classList.add('dark-mode')
+    document.getElementById('id-sun').classList.remove('active')
+    this.classList.add('active')
+}
+
+function lluvia() {
+
+    var sonido=document.getElementById("efecto");
+    
+    
+        if (sonido.paused) {
+            sonido.play();
+            
+         } else {
+    
+            sonido.pause(); 
+    }
+}
+
+/*function paralluvia() {
+    document.getElementById("efecto").pause();
+}*/
+
+
+function tema() {
+
+    var sonido2=document.getElementById("musiepica");
+
+
+    if (sonido2.paused) {
+        sonido2.play();
+        
+     } else {
+
+        sonido2.pause(); 
+}
+}
